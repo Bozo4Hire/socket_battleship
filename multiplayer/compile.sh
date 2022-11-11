376f69837bb370da5344c7fcb606d706e3cea7c0
Chabot18 <nor.guzcha@gmail.com>
2022-11-10 22:04:35 -0800

    Renaming .exe files, compiling script modified

--- multiplayer/compile.sh
+++ multiplayer/compile.sh
@@ -2,5 +2,5 @@
 # Compiler for the main programs from this repo
 # v1
 
-g++ client.cpp -o bb_client.exe 
-g++ server.cpp -o bb_server.exe
+g++ client.cpp -o client.exe 
+g++ server.cpp -o server.exe
